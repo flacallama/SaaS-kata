16e0d93237d4698dc53da23af30ec4874bf68719
Dylan Cunningham <gdylanc@gmail.com>
2017-11-28 09:32:42 -0700

    dependencies added

--- code-kata/src/index.js
+++ code-kata/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import Store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let store = new Store();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
