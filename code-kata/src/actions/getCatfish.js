27eb74ba18e4fdf486f48b6c97b9b4eb6bef345e
Dylan Cunningham <gdylanc@gmail.com>
2017-11-28 10:02:36 -0700

    action getCatfish completed. Will request an array of 100 catfish catches

---
+++ code-kata/src/actions/getCatfish.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const getCatfish = () => {
+  console.log("action: getCatfish was dispatched");
+  return {
+    type: "GET_CATFISH",
+    payload: axios.get(`https://868r1t0.restletmocks.net/catfish`)
+  }
+}
